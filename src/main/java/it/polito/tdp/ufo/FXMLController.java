04a89020280d5101a5a2cff15234c0b153cdbbcc
Fulvio Corno <fulvio.corno@polito.it>
2021-03-22 11:55:13 +0100

    Aggiunta del Model e dell'interfaccia grafica

--- src/main/java/it/polito/tdp/ufo/FXMLController.java
+++ src/main/java/it/polito/tdp/ufo/FXMLController.java
@@ -2,24 +2,46 @@ package it.polito.tdp.ufo;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+
+import it.polito.tdp.ufo.model.Model;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 
-public class FXMLController implements Initializable {
-    
+public class FXMLController {
+	
+	private Model model ;
+
     @FXML
-    private Label label;
-    
+    private ResourceBundle resources;
+
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+    private URL location;
+
+    @FXML
+    private ComboBox<String> boxForma;
+
+    @FXML
+    private Label txtMessaggio;
+
+    @FXML
+    void handleConta(ActionEvent event) {
+    	String forma = boxForma.getValue();
+    	int count = model.getCountByForma(forma);
+    	txtMessaggio.setText("Gli UFO della forma "+forma+" sono: "+count);
+
     }
     
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+    public void setModel(Model m) {
+    	this.model = m ;
+    	boxForma.getItems().addAll(this.model.getFormeUFO()) ;
+    }
+
+    @FXML
+    void initialize() {
+        assert boxForma != null : "fx:id=\"boxForma\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtMessaggio != null : "fx:id=\"txtMessaggio\" was not injected: check your FXML file 'Scene.fxml'.";
+
+    }
 }
--- src/main/java/it/polito/tdp/ufo/model/Model.java
+++ src/main/java/it/polito/tdp/ufo/model/Model.java
@@ -2,7 +2,22 @@ package it.polito.tdp.ufo.model;
 
 import java.util.List;
 
+import it.polito.tdp.ufo.db.SightingDAO;
+
 public class Model {
 
-	private List<String> formeUFO ;
+	private List<String> formeUFO = null ;
+	
+	public List<String> getFormeUFO() {
+		if (this.formeUFO==null) {
+			SightingDAO dao = new SightingDAO() ;
+			this.formeUFO = dao.readShapes() ;
+		}
+		return this.formeUFO;
+	}
+	
+	public int getCountByForma(String forma) {
+		SightingDAO dao = new SightingDAO();
+		return dao.countByShape(forma) ;
+	}
 }
